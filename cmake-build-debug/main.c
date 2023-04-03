9d99ef1389d97d703a674eabb37aa28edc51c247
Boris Victoria <boris.v.victoria@gmail.com>
2023-04-03 23:07:25 +0800

    Add main.c

---
+++ cmake-build-debug/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "structures.c"
+#include "register.c"
+#include "user.c"
+#include "buy.c"
+#include "sell.c"
+#include "checkout.c"
+#include "admin.c"
+#include "helper.c"
+
+int main()
+{
+    printf("CCPROG2 MACHINE PROJECT\n");
+    printf("Made by: Jose Carlos Regala, Boris Victoria\n");
+    return 0;
+}
