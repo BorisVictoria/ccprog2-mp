49182e55db1768ae37cffd35a128661d4b3ea0ee
Carlos Regala <josecarlosregala@gmail.com>
2023-04-09 21:21:53 +0800

    showShopaholics finalized

--- admin.c
+++ admin.c
@@ -373,7 +373,186 @@ void showSellerSales(struct transaction transactions[], int transactionCount, st
 }
 
 void showShopaholics(struct transaction transactions[], int transactionCount, struct user users[], int userCount){
+    struct transactor transactors[100];
 
+    long month, month2;
+    long day, day2;
+    long year, year2;
+    long temp;
+    int valid, swap, foundIndex;
+    int ctr=0;
+
+    printf("Please input the start date:\n");
+    do
+    {
+        printf("Input month [mm]:");
+        do
+        {
+            month = getLong();
+            if (month < 1 || month > 12)
+                printf("Please input a valid month:");
+        }
+        while(month < 1 || month > 12);
+        printf("Input day [dd]:");
+        do
+        {
+            day = getLong();
+            if (day < 1 || day > 31)
+                printf("Please input a valid day:");
+        }
+        while(day < 1 || day > 31);
+        printf("Input year [yyyy]:");
+        do
+        {
+            year = getLong();
+            if (year < 0)
+                printf("Please input a valid year:");
+        }
+        while(year < 0);
+    }
+    while (checkValidDate(month, day, year) == 0);
+
+    printf("Please input the end date:\n");
+    do
+    {
+        printf("Input month [mm]:");
+        do
+        {
+            month2 = getLong();
+            if (month2 < 1 || month2 > 12)
+                printf("Please input a valid month:");
+        }
+        while(month2 < 1 || month2 > 12);
+        printf("Input day [dd]:");
+        do
+        {
+            day2 = getLong();
+            if (day2 < 1 || day2 > 31)
+                printf("Please input a valid day:");
+        }
+        while(day2 < 1 || day2 > 31);
+        printf("Input year [yyyy]:");
+        do
+        {
+            year2 = getLong();
+            if (year2 < 0)
+                printf("Please input a valid year:");
+        }
+        while(year2 < 0);
+    }
+    while (checkValidDate(month2, day2, year2) == 0);
+
+    if(year2<year){
+        swap = 1;
+    }
+    else if(year2==year){
+        if(month2<month){
+            swap=1;
+        }
+        else if(month2==month){
+            if(day2<day)
+                swap=1;
+            else
+                swap=0;
+        }
+        else
+            swap=0;
+    }
+    else
+        swap=0;
+
+    if(swap) {
+
+        temp = day;
+        day = day2;
+        day2 = temp;
+
+        temp = month;
+        month = month2;
+        month2 = temp;
+
+        temp = year;
+        year = year2;
+        year2 = temp;
+        printf("The end date inputted is earlier than the start date. The two dates have been swapped.\n");
+    }
+
+    for(int i=0; i<transactionCount; i++){
+        valid = 0;
+
+        if(transactions[i].year>year){
+            valid = 1;
+        }
+        else if(transactions[i].year==year){
+            if(transactions[i].month>month){
+                valid=1;
+            }
+            else if(transactions[i].month==month){
+                if(transactions[i].day>=day){
+                    valid=1;
+                }
+                else
+                    valid=0;
+            }
+            else
+                valid=0;
+        }
+        else
+            valid=0;
+
+        if(valid ==1){
+            if(transactions[i].year<year2){
+                valid = 1;
+            }
+            else if(transactions[i].year==year2){
+                if(transactions[i].month<month2){
+                    valid=1;
+                }
+                else if(transactions[i].month==month2){
+                    if(transactions[i].day<=day2)
+                        valid=1;
+                    else
+                        valid=0;
+                }
+                else
+                    valid=0;
+            }
+            else
+                valid=0;
+        }
+
+        if(valid){
+            foundIndex=-1;
+            for(int j=0; j<ctr; j++){
+                if(transactors[j].buyerid == transactions[i].buyerid) {
+                    foundIndex = j;
+                    j=100;
+                }
+            }
+            if(foundIndex==-1){
+                transactors[ctr].buyerid = transactions[i].buyerid;
+                for(int l=0; l<userCount; l++){
+                    if(transactions[i].buyerid == users[l].userid){
+                        strcpy(transactors[ctr].name, users[l].name);
+                    }
+                }
+                transactors[ctr].total = transactions[i].total;
+                ctr++;
+            }
+            else{
+                transactors[foundIndex].total += transactions[i].total;
+            }
+        }
+    }
+
+    //seller ID, seller name, total sales in the duration
+    printf("-------------------------------------------------------------------------------\n");
+    printf("|  buyerID   |         name           |  total amount bought in the duration  |\n");
+    printf("-------------------------------------------------------------------------------\n");
+    for(int k=0; k<ctr; k++) {
+        printf("| %10ld | %-22s | %37lf |\n", transactors[k].buyerid, transactors[k].name, transactors[k].total);
+        printf("-------------------------------------------------------------------------------\n");
+    }
 }
 
 void adminMenu(struct user users[], int userCount)
